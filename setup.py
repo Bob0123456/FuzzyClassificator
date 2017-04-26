676e0cc68cb953a7aacdd0634e4cc781cc0cd238
Timur Gilmullin <tgilmullin@ptsecurity.com>
2017-04-27 00:44:22 +0300

    #3: remove non-used setup.cfg

--- setup.py
+++ setup.py
@@ -14,6 +14,8 @@ setup(
 
     description='This program uses neural networks to solve classification problems, and uses fuzzy sets and fuzzy logic to interpreting results.',
 
+    long_description=open('README.md').read(),
+
     license='MIT',
 
     author='Timur Gilmullin',
