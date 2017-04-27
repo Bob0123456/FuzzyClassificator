882629db2411f5496310b787a6dd8ec66158b69d
Timur Gilmullin <tgilmullin@ptsecurity.com>
2017-04-27 13:17:30 +0300

    #3: debug commit for readthedocs service

--- conf.py
+++ conf.py
@@ -1,7 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import os
+import shutil
 from recommonmark.parser import CommonMarkParser
+
+if not os.path.exists('docs'):
+    os.mkdir('docs')
+
+shutil.copy('README.md', 'docs')
+
 
 source_parsers = {
     '.md': CommonMarkParser,
 }
 
-source_suffix = ['.md']
+source_suffix = ['.rst', '.md']
